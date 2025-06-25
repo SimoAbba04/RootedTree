e4747d89ca26f6574192ba171d5bcfb7cb573848
SimoAbba04 <s.abbatiello@studenti.unisa.it>
2025-06-25 18:25:34 +0200

    Riduzione accoppiamento

--- src/main/java/it/unisa/control/Registration.java
+++ src/main/java/it/unisa/control/Registration.java
@@ -1,5 +1,4 @@
 package it.unisa.control;
-
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -44,13 +43,13 @@ public class Registration extends HttpServlet {
 		if (data == null || data.trim().isEmpty()) {
 			System.out.println("Data nullo.");
 		}
-		String hashedPw = toHash(password);
+		String hashedPw = HtmlHash.toHash(password);
 		Date dataNascita = Date.valueOf(data);
 		System.out.println("  " + hashedPw);// Debug
 		UserDTO user = new UserDTO();
-		user.setNome(encodeHtmlEntities(name));
-		user.setCognome(encodeHtmlEntities(cognome));
-		user.setEmail(encodeHtmlEntities(email));
+		user.setNome(HtmlDecoder.encodeHtmlEntities(name));
+		user.setCognome(HtmlDecoder.encodeHtmlEntities(cognome));
+		user.setEmail(HtmlDecoder.encodeHtmlEntities(email));
 		user.setPassword(hashedPw);
 		user.setDataNascita(dataNascita);
 
@@ -74,28 +73,5 @@ public class Registration extends HttpServlet {
 			request.setAttribute("messaggio", "Errore durante la registrazione: " + e.getMessage());
 			request.getRequestDispatcher("/registrazione.jsp").forward(request, response);
 		}
-	}
-
-	private String toHash(String password) {
-		String hashString = null;
-		try {
-			java.security.MessageDigest digest = java.security.MessageDigest.getInstance("SHA-512");
-			byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-			hashString = "";
-			for (int i = 0; i < hash.length; i++) {
-				hashString += Integer.toHexString((hash[i] & 0xFF) | 0x100).substring(1, 3);
-			}
-		} catch (java.security.NoSuchAlgorithmException e) {
-			System.out.println(e);
-		}
-		return hashString;
-	}
-
-	public static String encodeHtmlEntities(String input) {
-		if (input == null)
-			return null;
-
-		return input.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;")
-				.replace("'", "&#39").replace(" ", "&nbsp;");
 	}
 }
